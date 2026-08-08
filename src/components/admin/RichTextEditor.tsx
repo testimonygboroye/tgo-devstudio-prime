a900a42ce53d44053f3840d4cdde06d4379b315c
Testimony Gboroye <testimonygboroye.dev@gmail.com>
2026-08-08 04:21:49 +0100

    Fix Tiptap v3 TextStyle named export

--- src/components/admin/RichTextEditor.tsx
+++ src/components/admin/RichTextEditor.tsx
@@ -3,7 +3,7 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import TextAlign from "@tiptap/extension-text-align";
-import TextStyle from "@tiptap/extension-text-style";
+import { TextStyle } from "@tiptap/extension-text-style";
 import Color from "@tiptap/extension-color";
 import Underline from "@tiptap/extension-underline";
 import Link from "@tiptap/extension-link";
